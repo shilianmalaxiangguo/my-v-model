847b6763de9c035134f40c5bfbf5b727cb3d0063
shilianmalaxiangguo <695614960@qq.com>
2020-03-24 16:37:42 +0800

    :tada:v-model的组件封装，可能以后有其他巧妙封装的组件实现

---
+++ README.md
@@ -0,0 +1,21 @@
+# test-model
+通过用`v-model`语法糖来控制组件的显隐，
+以前都是用props或者vuex或者mixin来控制，
+发现用语法糖实现起来更为方便，开箱即用
+## Project setup
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn serve
+```
+
+### Compiles and minifies for production
+```
+yarn build
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/components/my-modal.vue
@@ -0,0 +1,52 @@
+<template>
+  <div >
+       <Modal
+        v-model="the_show"
+        title="Common Modal dialog box title"
+        @on-ok="ok"
+        @on-cancel="cancel">
+        <p>Content of dialog</p>
+    </Modal>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'myModal',
+    model:{
+        prop: 'show',            
+        event: 'changed'       
+    },
+    data() {
+        return {
+            the_show:this.show
+        }
+    },
+    watch:{
+        show(newVal) {
+            this.the_show = newVal
+        }
+    },
+    props:{
+        show:{
+            type:Boolean,
+            default:() => false
+        }
+    },
+    methods:{
+       ok() {
+            this.$emit('changed',false)
+            this.$emit('on-ok')
+       },
+        cancel() {
+            this.$emit('changed',false)
+            this.$emit('on-close')
+
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
